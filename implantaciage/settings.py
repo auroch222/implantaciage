f9f338308df3709d236c57a0da0a7fc513109d81
auroch222 <auroch222@gmail.com>
2016-07-06 17:32:20 +0400

    Update settings.py

--- implantaciage/settings.py
+++ implantaciage/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i_q3&lzm!t8s+d!q6$m3ix$&)&$3w2quyodq8$kd*lkq_)5)um'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'implantaciage.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'implantaciage',
+        'NAME': 'implantaciagelast',
         'USER': 'auroch',
         'PASSWORD': 'iraklisparoli1995',
         'HOST': 'localhost',
