69ec8d47642b44c13f281a44ad97e8abb4422dda
auroch222 <auroch222@gmail.com>
2016-07-06 18:53:54 +0400

    Update settings.py

--- implantaciage/settings.py
+++ implantaciage/settings.py
@@ -143,8 +143,7 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
+STATIC_ROOT = '/opt/myproject/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
